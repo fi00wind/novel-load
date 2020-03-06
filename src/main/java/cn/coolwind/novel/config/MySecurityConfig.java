ae4986f4d065bcd96b71439ccb6c042e757a4551
fi00wind <259560371@qq.com>
2020-03-06 20:15:08 +0800

    remember me

--- src/main/java/cn/coolwind/novel/config/MySecurityConfig.java
+++ src/main/java/cn/coolwind/novel/config/MySecurityConfig.java
@@ -1,5 +1,9 @@
 package cn.coolwind.novel.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,17 +11,34 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+//        tokenRepository.setCreateTableOnStartup(true);
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").authenticated()
-                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
         .and()
+                .rememberMe().tokenRepository(persistentTokenRepository())
+                .and()
         .formLogin();
     }
 
@@ -25,8 +46,8 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .passwordEncoder(new BCryptPasswordEncoder())
-                .withUser("F")
-                .password(new BCryptPasswordEncoder().encode("fff@111"))
+                .withUser("G")
+                .password(new BCryptPasswordEncoder().encode("51~GF"))
                 .roles("admin");
     }
 
